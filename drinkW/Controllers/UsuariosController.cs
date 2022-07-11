1b478be85b0a1cb5bf7d9bd7bf2fe7820f0abe87
Arthur Almeida <68932243+thuralmeida@users.noreply.github.com>
2022-07-10 21:04:21 -0300

    Padronização do layout dos botões e links. Término da criação dos Views do Usuário.

---
+++ drinkW/Controllers/UsuariosController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using drinkW.Models;
+
+namespace drinkW.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly Context _context;
+
+        public UsuariosController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Usuarios
+        public async Task<IActionResult> Index()
+        {
+              return _context.Usuario != null ? 
+                          View(await _context.Usuario.ToListAsync()) :
+                          Problem("Entity set 'Context.Usuario'  is null.");
+        }
+
+        // GET: Usuarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Usuario == null)
+            {
+                return NotFound();
+            }
+
+            var Usuario = await _context.Usuario
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (Usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(Usuario);
+        }
+
+        // GET: Usuarios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Usuarios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Login,Senha,Email")] Usuario Usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(Usuario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Usuario);
+        }
+
+        // GET: Usuarios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Usuario == null)
+            {
+                return NotFound();
+            }
+
+            var Usuario = await _context.Usuario.FindAsync(id);
+            if (Usuario == null)
+            {
+                return NotFound();
+            }
+            return View(Usuario);
+        }
+
+        // POST: Usuarios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Login,Senha,Email")] Usuario Usuario)
+        {
+            if (id != Usuario.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(Usuario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UsuarioExists(Usuario.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Usuario);
+        }
+
+        // GET: Usuarios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Usuario == null)
+            {
+                return NotFound();
+            }
+
+            var Usuario = await _context.Usuario
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (Usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(Usuario);
+        }
+
+        // POST: Usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Usuario == null)
+            {
+                return Problem("Entity set 'Context.Usuario'  is null.");
+            }
+            var Usuario = await _context.Usuario.FindAsync(id);
+            if (Usuario != null)
+            {
+                _context.Usuario.Remove(Usuario);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UsuarioExists(int id)
+        {
+          return (_context.Usuario?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
